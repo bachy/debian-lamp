29b798b58d0aebc05cabc73a68cc5158ce1b7d02
Bachir Soussi Chiadmi <bachir@g-u-i.me>
2017-01-23 14:39:00 +0100

    apticron install

--- install-debian-server.sh
+++ install-debian-server.sh
@@ -529,6 +529,35 @@ echo "\033[92;1mDot files installed for root, you should installed them manually
 
 
 echo '\033[35m
+    ___         __           __  __          __      __
+   /   | __  __/ /_____     / / / /___  ____/ /___ _/ /____
+  / /| |/ / / / __/ __ \   / / / / __ \/ __  / __ `/ __/ _ \
+ / ___ / /_/ / /_/ /_/ /  / /_/ / /_/ / /_/ / /_/ / /_/  __/
+/_/  |_\__,_/\__/\____/   \____/ .___/\__,_/\__,_/\__/\___/
+                              /_/
+\033[0m'
+# https://www.howtoforge.com/how-to-configure-automatic-updates-on-debian-wheezy
+# https://www.bisolweb.com/tutoriels/serveur-vps-ovh-partie-5-installation-apticron/
+
+echo "\033[35;1mInstalling apticron \033[0m"
+apt-get install apticron
+
+sleep 3
+echo -n "Enter an email: "
+read email
+
+sed -ir "s/EMAIL=\"root\"/EMAIL=\"$email\"/g" /etc/apticron/apticron.conf
+# sed -ir "s/# DIFF_ONLY=\"1\"/DIFF_ONLY=\"1\"/g" /etc/apticron/apticron.conf
+sed -ir "s/# NOTIFY_NEW=\"0\"/NOTIFY_NEW=\"0\"/g" /etc/apticron/apticron.conf
+
+
+
+
+echo "\033[92;1mApticron installed and configured\033[0m"
+
+
+
+echo '\033[35m
                   __
   ___  ____  ____/ /
  / _ \/ __ \/ __  /
